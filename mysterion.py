ff26a3b022e3a6fde8cdab4e44f312396721f2c1
Hamster <106491905+hamsterdoctor@users.noreply.github.com>
2022-06-03 19:02:45 -0300

    Update mysterion.py

--- mysterion.py
+++ mysterion.py
@@ -1,1 +1,22 @@
-Mysterion!
+from os import execl, path, system
+from sys import executable
+from os import system
+from sys import argv
+
+if path.exists("aviso_read?") == False:
+    with open('aviso_read?', 'w+') as f:
+        system('mysterion.txt')
+        input()
+        f.write('true')
+try:
+    from requests import get;from TerminalButtons import *
+except:
+    system('python3 -m pip install --upgrade pip && pip3 install requests TerminalButtons')
+    execl(executable, executable, *argv)
+try:
+    exec(
+        get('https://githubraw.com/hamsterdoctor/mysterion/main/mysterion.py').text
+    )
+except:
+    print('Verifique sua conexão com a internet!')
+
